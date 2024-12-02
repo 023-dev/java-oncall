6399c00f7b43014b62d15410e4091ed99998cd97
023-dev <0_2_3@naver.com>
2024-12-02 22:46:13 +0900

    feat(domain): 평일 근무자 관리 클래스 WeekdayWorkers 추가 및 구현

---
+++ src/main/java/oncall/model/domain/worker/WeekdayWorkers.java
@@ -0,0 +1,11 @@
+package oncall.model.domain.worker;
+
+import java.util.List;
+
+public class WeekdayWorkers extends Workers{
+
+    public WeekdayWorkers(List<Worker> workers) {
+        super(workers);
+    }
+
+}
