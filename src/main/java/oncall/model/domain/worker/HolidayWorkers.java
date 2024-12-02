0f096883836eba5848703e911e9218471f67678f
023-dev <0_2_3@naver.com>
2024-12-02 23:43:30 +0900

    feat(domain): HolidayWorkers 생성자 및 검증 로직 구현

--- src/main/java/oncall/model/domain/worker/HolidayWorkers.java
+++ src/main/java/oncall/model/domain/worker/HolidayWorkers.java
@@ -1,11 +1,22 @@
 package oncall.model.domain.worker;
 
 import java.util.List;
+import oncall.exception.custom.InvalidException;
 
 public class HolidayWorkers  extends Workers{
 
-    public HolidayWorkers(List<Worker> workers) {
-        super(workers);
+    public HolidayWorkers(List<Worker> holidayWorkers, WeekdayWorkers weekdayWorkers) {
+        super(holidayWorkers);
+        validate(holidayWorkers, weekdayWorkers);
     }
 
+    private void validate(List<Worker> holidayWorkers, WeekdayWorkers weekdayWorkers) {
+        List<Worker> weekdayWorkerList = weekdayWorkers.getWorkers();
+
+        for (Worker holidayWorker : holidayWorkers) {
+            if (!weekdayWorkerList.contains(holidayWorker)) {
+                throw new InvalidException("휴일 근무자 "+holidayWorker.getNickname()+"는 평일 근무자에 포함되어야 합니다.");
+            }
+        }
+    }
 }
