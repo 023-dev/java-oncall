147fbab9ba0a90462991ba6875a99adaf4f778a6
023-dev <0_2_3@naver.com>
2024-12-02 22:46:30 +0900

    feat(domain): 휴일 근무자 관리 클래스 HolidayWorkers 추가 및 구현

---
+++ src/main/java/oncall/model/domain/worker/HolidayWorkers.java
@@ -0,0 +1,11 @@
+package oncall.model.domain.worker;
+
+import java.util.List;
+
+public class HolidayWorkers  extends Workers{
+
+    public HolidayWorkers(List<Worker> workers) {
+        super(workers);
+    }
+
+}
