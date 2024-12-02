dda95554a3fb6cede991f9a3a9dde4e4d15e2609
023-dev <0_2_3@naver.com>
2024-12-02 22:48:53 +0900

    feat(main): 시작점 main 메서드에 config 및 controller 초기화 로직 추가

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,13 +1,10 @@
 package oncall;
 
+import oncall.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig app = new AppConfig();
+        app.run();
     }
-}
-
-public record class mainDto {
-    private int id;
-    private String name;
-    private int age;
 }
