b4fa241b263cc62ac493e2c390b5a78b39605da8
023-dev <0_2_3@naver.com>
2024-12-03 17:42:16 +0900

    feat(domain): 근무 관리 클래스 Schedule 구현

--- src/main/java/oncall/model/domain/schedule/Schedule.java
+++ src/main/java/oncall/model/domain/schedule/Schedule.java
@@ -1,17 +1,18 @@
 package oncall.model.domain.schedule;
 
+import java.util.Collections;
 import java.util.List;
 import oncall.exception.custom.InvalidException;
 
-public class OnCallMonth {
+public class Schedule {
     private static final int MIN_MONTH = 1;
     private static final int MAX_MONTH = 12;
-    private static final List<String> DAYS_OF_WEEK = List.of("일", "월", "화", "수", "목", "금", "토");
+    public static final List<String> DAYS_OF_WEEK = Collections.unmodifiableList(List.of("일", "월", "화", "수", "목", "금", "토"));
 
     private final int month;
     private final String startDay;
 
-    public OnCallMonth(String month, String startDay) {
+    public Schedule(String month, String startDay) {
         this.month = validateMonth(month);
         this.startDay = validateStartDay(startDay);
     }
@@ -35,7 +36,7 @@ public class OnCallMonth {
         return startDay;
     }
 
-    public int getDatsOfMonth() {
+    public int getDaysOfMonth() {
         return switch (month) {
             case 2 -> 28;
             case 4, 6, 9, 11 -> 30;
@@ -44,12 +45,30 @@ public class OnCallMonth {
     }
 
     public String getDayOfWeek(int day) {
-        if (day < 1 || day > getDatsOfMonth()) {
-            throw new InvalidException("잘못된 날짜입니다. 1일에서 " + getDatsOfMonth() + "일 사이여야 합니다.");
+        if (day < 1 || day > getDaysOfMonth()) {
+            throw new InvalidException("잘못된 날짜입니다. 1일에서 " + getDaysOfMonth() + "일 사이여야 합니다.");
         }
         int startDayIndex = DAYS_OF_WEEK.indexOf(startDay);
-        return DAYS_OF_WEEK.get((startDayIndex+1)%7);
+        return DAYS_OF_WEEK.get((startDayIndex-1)%7);
     }
+
+    public boolean isWeekend(String currentDay) {
+        return currentDay.equals("토") || currentDay.equals("일");
+    }
+
+    public boolean isHoliday(int day, String currentDay) {
+        return Holiday.isHoliday(this.month, day) || isWeekend(currentDay);
+    }
+
+    public String getHoliday(int day) {
+        for (Holiday holiday : Holiday.values()) {
+            if (holiday.getMonth() == this.month && holiday.getDay() == day) {
+                return holiday.getName();
+            }
+        }
+        return null; // 휴일이 아닌 경우
+    }
+
 
     public int getMonth() {
         return month;
